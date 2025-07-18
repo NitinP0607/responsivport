aa37007d98b118fb24ad4e2aa05a2f6a95d09479
Nitin Patel <156987426+nitin-pat@users.noreply.github.com>
2025-07-18 13:16:36 +0530

    Ignore node_modules

---
+++ public/script.js
@@ -0,0 +1,28 @@
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
+
+menuIcon.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+  const form = document.getElementById("contactForm");
+
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const data = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+
+    const res = await fetch("http://localhost:8080/send", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    const result = await res.json();
+    alert(result.message);
+    form.reset();
+  });
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/send", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "patelnitin1299@gmail.com",        // replace with your Gmail
+      pass: "wtkd yzwh wann gnhg",           // generate an "App Password" 
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: "patelnitin1299@gmail.com",            // where to receive the message
+    subject: "New Contact Form Message",
+    text: `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "You Message sent successfully!" });
+  } catch (error) {
+    console.error("Email error:", error);
+    res.status(500).json({ message: "Email failed to send" });
+  }
+});
+
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
